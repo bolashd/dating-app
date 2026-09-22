f9b417dbe4d2e91136356fc62c46846851941cc1
Dane Bolash <bolashd@gmail.com>
2026-09-21 20:40:34 -0400

    End of lesson 161

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -12,9 +12,11 @@ namespace API.Controllers;
 public class MembersController(IMemberRepository memberRepository, IPhotoService photoService) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers([FromQuery] PagingParams pagingParams)
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers([FromQuery] MemberParams memberParams)
     {
-        return Ok(await memberRepository.GetMembersAsync(pagingParams));
+        memberParams.CurrentMemberId = User.GetMemberId();
+
+        return Ok(await memberRepository.GetMembersAsync(memberParams));
     }
 
     [HttpGet("{id}")]
